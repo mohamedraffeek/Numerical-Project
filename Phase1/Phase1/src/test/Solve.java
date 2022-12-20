77229a3b3f9c6f8071bf9cce3cd024bdfe4f3509
Mohamed Raffeek <mohamedraffeek2001@gmail.com>
2022-12-20 18:06:34 +0200

    a

--- Phase1/Phase1/src/test/Solve.java
+++ Phase1/Phase1/src/test/Solve.java
@@ -46,7 +46,8 @@ public class Solve {
 						break;
 					}
 					case "Cholesky" : {
-						
+						Cholesky obj = new Cholesky(specialCoef, b, significantDigits);
+						ans = obj.Solve();
 						break;
 					}
 				}
